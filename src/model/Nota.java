507cec7aee7b3c3d5bf769275e76e9c9e12607c3
JmRivGonza <Jonaymartinrivero@gmail.com>
2026-02-28 11:19:15 +0000

    Creacion  de las clases Nota y Usuario

--- src/model/Nota.java
+++ src/model/Nota.java
@@ -1,5 +1,19 @@
 package model;
 
 public class Nota {
-    
+    private String titulo;
+    private String contenido;
+
+    public Nota(String titulo, String contenido) {
+        this.titulo = titulo;
+        this.contenido = contenido;
+    }
+
+    public String getTitulo() { return titulo; }
+    public String getContenido() { return contenido; }
+
+    @Override
+    public String toString() {
+        return titulo + ";" + contenido;
+    }
 }
--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,6 +1,23 @@
 package model;
 
 public class Usuario {
-    
+    private String email;
+    private String contraseña;
+
+    public Usuario(String email, String contraseña) {
+        this.email = email;
+        this.contraseña = contraseña;
+    }
+
+    public String getEmail() { return email; }
+    public String getContraseña() { return contraseña; }
+
+    public String getFormatoEmail() {
+        return email.replaceAll("[^a-zA-Z0-9]", "_");
+    }
+
+    @Override
+    public String toString() {
+        return email + ";" + contraseña;
     }
 }
